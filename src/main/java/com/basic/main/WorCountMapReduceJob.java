4fd2da09142ae95899999f285710b97f91f15e17
dell-pc <798750509@qq.com>
2017-03-28 22:05:03 +0800

    2017/3/28 21：34  修正代码

--- src/main/java/com/basic/main/WorCountMapReduceJob.java
+++ src/main/java/com/basic/main/WorCountMapReduceJob.java
@@ -53,7 +53,7 @@ public class WorCountMapReduceJob {
                 InterruptedException {
             conf = context.getConfiguration();
             caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
-            if (conf.getBoolean("wordcount.skip.patterns", true)) {
+            if (conf.getBoolean("wordcount.skip.patterns", false)) {
                 URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
                 for (URI patternsURI : patternsURIs) {
                     Path patternsPath = new Path(patternsURI.getPath());
